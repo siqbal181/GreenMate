e11100fb5ffb8f62e74dac51fee051fac8e107d1
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-15 19:06:52 +0100

    conditional navbar/bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Register } from './pages/Register/Register';
 import Homepage from './pages/Homepage';
 import Navbar from './components/Navbar';
@@ -12,17 +12,29 @@ const userData = {
   monthAvg: 17
 }
 
+const AppContent = () => {
+  const location = useLocation();
+
+  const showNavOrBottom = location.pathname !== '/register';
+
+  return (
+    <>
+      {showNavOrBottom && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Homepage userData={userData} />} />
+        <Route path='/daily-planning' element={<DailyPlanning />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+      {showNavOrBottom && <BottomBar />}
+    </>
+  );
+};
+
 function App() {
   return (
     <div className='app'>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Homepage userData={userData} />} />
-          <Route path='/daily-planning' element={<DailyPlanning/>} />
-          <Route path='/register' element={<Register/>}/>
-        </Routes>
-        <BottomBar />
+        <AppContent />
       </BrowserRouter>
     </div>
   );
