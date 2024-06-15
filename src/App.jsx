bf6cfcad130bded5d20498f0bc7df8fd75f0b786
Pablo <pablito@me.com>
2024-06-15 14:41:28 +0100

    Added homepage, navbar and bottombar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Navbar from './components/Navbar';
+import BottomBar from './components/BottomBar';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Navbar /> */}
+        <Navbar />
         <Routes>
-          {/* <Route path='/' element={<HomePage />} /> */}
+          <Route path='/' element={<Homepage />} />
         </Routes>
+        <BottomBar />
       </BrowserRouter>
     </>
   );
---
+++ src/components/BottomBar.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const BottomBar = () => {
+  return (
+    <div>
+      BottomBar
+    </div>
+  )
+}
+
+export default BottomBar
