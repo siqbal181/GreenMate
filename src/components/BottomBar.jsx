b872f8e7f76ce7bcf19873c468777563fc7c441c
Pablo <pablito@me.com>
2024-06-15 22:28:20 +0100

    Various fixes.

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -5,14 +5,15 @@ import { IoHomeOutline } from "react-icons/io5";
 import { BsCardChecklist } from "react-icons/bs";
 import { MdOutlineLeaderboard } from "react-icons/md";
 import { LiaAwardSolid } from "react-icons/lia";
+const size = 38;
 
 const BottomBar = () => {
   return (
     <footer className='bottom-bar'>
-      <Link to='/' className='nav-link'><IoHomeOutline size={28} /></Link>
-      <Link to='/daily-planning' className='nav-link'><BsCardChecklist size={28} /></Link>
-      <Link to='/leaderboard' className='nav-link'><MdOutlineLeaderboard size={28} /></Link>
-      <Link to='/achievements' className='nav-link'><LiaAwardSolid size={28} /></Link>
+      <Link to='/' className='nav-link'><IoHomeOutline size={size} /></Link>
+      <Link to='/daily-planning' className='nav-link'><BsCardChecklist size={size} /></Link>
+      <Link to='/leaderboard' className='nav-link'><MdOutlineLeaderboard size={size} /></Link>
+      <Link to='/achievements' className='nav-link'><LiaAwardSolid size={size} /></Link>
     </footer>
   )
 }
