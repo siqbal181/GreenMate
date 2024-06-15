e4e1e027e0d0265a18726ceeb8f0be74d7f1d4cc
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-15 15:55:23 +0100

    component for daily questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Register } from './pages/Register/Register';
+import { DailyQuestions } from './components/DailyQuestions/DailyQuestions';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <DailyQuestions/>
         {/* <Navbar /> */}
         <Routes>
           <Route path='/register' element={<Register/>}/>
