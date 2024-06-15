1b9bd4e15595e160f29e07ab5711465cb9211532
Pablo <pablito@me.com>
2024-06-15 20:24:02 +0100

    checklist added to route

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -10,7 +10,7 @@ const BottomBar = () => {
   return (
     <footer className='bottom-bar'>
       <Link to='/' className='nav-link'><IoHomeOutline size={28} /></Link>
-      <Link to='/checklist' className='nav-link'><BsCardChecklist size={28} /></Link>
+      <Link to='/daily-planning' className='nav-link'><BsCardChecklist size={28} /></Link>
       <Link to='/leaderboard' className='nav-link'><MdOutlineLeaderboard size={28} /></Link>
       <Link to='/achievements' className='nav-link'><LiaAwardSolid size={28} /></Link>
     </footer>
