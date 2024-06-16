93110a415db3b0749d4a56a244ca6517986ee557
dev4coder <vinnu2203@gmail.com>
2024-06-16 11:47:27 +0100

    fixed the css of app bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 // import React from 'react'
+import { SiGoogleearth } from "react-icons/si";
+import { CgProfile } from "react-icons/cg";
 import './Navbar.css';
 
 // eslint-disable-next-line react/prop-types
 const Navbar = ({ setShowModal }) => {
   return (
     <nav className='navbar' onClick={() => setShowModal(true)}>
-      Navbar
+      <div className='navbar-item'>
+        <div className='nav-logo'>
+          <SiGoogleearth size={28}/>
+        </div>
+        <div className='nav-title'>Green Mate</div>
+        </div>
+      <div className='navbar-item'><CgProfile size={28}/></div>
     </nav>
   )
 }
