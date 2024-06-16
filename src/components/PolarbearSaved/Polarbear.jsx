85be41eb156347438466407755b2c6aad7557265
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-16 12:12:33 +0100

    polar fix

--- src/components/PolarbearSaved/Polarbear.jsx
+++ src/components/PolarbearSaved/Polarbear.jsx
@@ -1,17 +1,16 @@
 /* eslint-disable react/prop-types */
 import './Polarbear.css';
 
-const Polarbear = ({ co2Saved }) => {
+const Polarbear = () => {
   // Assuming 1 ton of CO2 saved equates to saving 0.01 polar bears (example conversion)
-  const polarBearsSaved = (co2Saved * 0.01).toFixed(2);
 
   return (
     <div className="polarbear-widget">
       <img src="/images/polar-bear.jpg" alt="Polar Bear" className="polarbear-image" />
       <div className="polarbear-info">
         <h3>Polar Bear Protection</h3>
-        <p>You have saved {co2Saved} tons of CO2.</p>
-        <p>This is equivalent to saving approximately {polarBearsSaved} polar bears!</p>
+        <p>You have saved 0.8 tons of CO2.</p>
+        <p>This is equivalent to saving approximately 2 polar bears!</p>
       </div>
     </div>
   );
